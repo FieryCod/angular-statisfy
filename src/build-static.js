938ae4e2ef274a6a007031b354c6f19b29f3f424
Karol Wójcik <karol.wojcik@tuta.io>
2020-02-28 21:34:49 +0100

    Fix indent & console.error invocation

--- src/build-static.js
+++ src/build-static.js
@@ -24,86 +24,86 @@ async function writeFile(filePath, contents) {
 }
 
 module.exports = {
-  Statisfy: {
-    async generateStaticHtml(config) {
-      console.log('\t[fn Statisfy#generateStaticHTML] Running fn');
+    Statisfy: {
+        async generateStaticHtml(config) {
+            console.log('\t[fn Statisfy#generateStaticHTML] Running fn');
 
-      const puppeteerArgs = [];
+            const puppeteerArgs = [];
 
-      if (config.sandBox === false) {
-        puppeteerArgs.push(...[
-          '--no-sandbox',
-          '--disable-setuid-sandbox',
-        ]);
-      }
+            if (config.sandBox === false) {
+                puppeteerArgs.push(...[
+                    '--no-sandbox',
+                    '--disable-setuid-sandbox',
+                ]);
+            }
 
-      console.log('\t[fn Statisfy#generateStaticHTML] Configuration: ' + JSON.stringify(config));
+            console.log('\t[fn Statisfy#generateStaticHTML] Configuration: ' + JSON.stringify(config));
 
-      const tries = config.tries || 3;
+            const tries = config.tries || 3;
 
-      console.log('\t[fn Statisfy#generateStaticHTML] launching browser..');
+            console.log('\t[fn Statisfy#generateStaticHTML] launching browser..');
 
-      try {
-        const browser = await puppeteer.launch({args: puppeteerArgs});
-        console.log('\t[fn Statisfy#generateStaticHTML] Browser launched!');
-
-        for (const route of config.routes) {
-          const fullRoute = config.host + route;
-
-          if (config.verbose !== false) {
-            console.log(`\t[fn Statisfy#generateStaticHTML] Processing route: ${fullRoute}`);
-          }
-
-          let html;
-          let success = false;
-          let tryCounter = 0;
-
-          while (!success && tryCounter++ <= tries) {
             try {
-              const page = await browser.newPage();
-              await page.goto(fullRoute);
-              html = await page.evaluate(() => document.documentElement.outerHTML);
-              console.log(`\t[fn Statisfy#generateStaticHTML] Route scrapped succesfully. Killing route!`);
-              await page.close();
-              success = true;
-            } catch (e) {
-              if (config.verbose !== false) {
-                console.warn(`\tCould not evaluate ${fullRoute} in`
-                             + ` try ${tryCounter}.`);
-                console.warn(`\tError: ${e}`);
-              }
+                const browser = await puppeteer.launch({args: puppeteerArgs});
+                console.log('\t[fn Statisfy#generateStaticHTML] Browser launched!');
+
+                for (const route of config.routes) {
+                    const fullRoute = config.host + route;
+
+                    if (config.verbose !== false) {
+                        console.log(`\t[fn Statisfy#generateStaticHTML] Processing route: ${fullRoute}`);
+                    }
+
+                    let html;
+                    let success = false;
+                    let tryCounter = 0;
+
+                    while (!success && tryCounter++ <= tries) {
+                        try {
+                            const page = await browser.newPage();
+                            await page.goto(fullRoute);
+                            html = await page.evaluate(() => document.documentElement.outerHTML);
+                            console.log(`\t[fn Statisfy#generateStaticHTML] Route scrapped succesfully. Killing route!`);
+                            await page.close();
+                            success = true;
+                        } catch (e) {
+                            if (config.verbose !== false) {
+                                console.warn(`\tCould not evaluate ${fullRoute} in`
+                                             + ` try ${tryCounter}.`);
+                                console.warn(`\tError: ${e}`);
+                            }
+                        }
+                    }
+
+                    if (!success) {
+                        console.error(`\tCould not evaluate ${fullRoute} in ${tryCounter} tries.`);
+                        continue;
+                    }
+
+                    const dom = cheerio.load(html);
+                    dom('script').each((index, item) => {
+                        if ('src' in item.attribs && !item.attribs.src.startsWith('http')) {
+                            dom(item).remove();
+                        }
+                    });
+
+                    dom('base').attr('href', config.host);
+
+                    const writeRoutePath = route.replace(`/${config.directory}/`, '');
+                    const fileName = `${(writeRoutePath && writeRoutePath !== '/' && writeRoutePath !== '') ? writeRoutePath : 'index'}.html`;
+
+                    console.error(`\t[fn Statisfy#generateStaticHTML] Generating file for route: ${route} -> ${config.directory}/${fileName[0] === '/' ? fileName.slice(1) : fileName}`);
+
+                    const editedDom = dom.html();
+                    await writeFile(`${config.directory}/${fileName}`, editedDom);
+                }
+
+                console.log(`\t[fn Statisfy#generateStaticHTML] Killing browser!`);
+
+                await browser.close();
+            } catch (err) {
+                console.error('\t' + err.message);
             }
-          }
-
-          if (!success) {
-            console.error(`\tCould not evaluate ${fullRoute} in ${tryCounter} tries.`);
-            continue;
-          }
-
-          const dom = cheerio.load(html);
-          dom('script').each((index, item) => {
-            if ('src' in item.attribs && !item.attribs.src.startsWith('http')) {
-              dom(item).remove();
-            }
-          });
-
-          dom('base').attr('href', config.host);
-
-          const writeRoutePath = route.replace(`/${config.directory}/`, '');
-          const fileName = `${(writeRoutePath && writeRoutePath !== '/' && writeRoutePath !== '') ? writeRoutePath : 'index'}.html`;
-
-          console.error(`\t[fn Statisfy#generateStaticHTML] Generating file for route: ${route} -> ./${config.directory}/${fileName}`);
-
-          const editedDom = dom.html();
-          await writeFile(`${config.directory}/${fileName}`, editedDom);
         }
-
-        console.log(`\t[fn Statisfy#generateStaticHTML] Killing browser!`);
-
-        await browser.close();
-      } catch (err) {
-        console.err('\t' + err.message);
-      }
     }
-  }
 };
